4fa87946d9879bc524dd4004653515002df5c673
safarov-en <safarov.elkhan110@gmail.com>
2024-05-30 15:10:52 +0500

    Adding a store

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Tasks from "./views/Tasks.vue";
-import New from "./views/New.vue";
-import Task from "./views/Task.vue";
+import Tasks from "../views/Tasks.vue";
+import New from "../views/New.vue";
+import Task from "../views/Task.vue";
 
 export default createRouter({
-    history: createWebHistory,
+    history: createWebHistory(),
     routes: [
         {
             path: '/',
